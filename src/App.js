9a062990edcddcecf77446db75e1a7c004e52636
guidopauletti <guido13pauletti@gmail.com>
2022-03-05 10:40:17 -0300

    agregando el About Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import { NavBar } from "./components/PageWrapper";
+import { PageWrapper } from "./components/PageWrapper";
+import { BrowserRouter , Route, Routes} from 'react-router-dom';
+import React from "react";
 
-function App() {
+// Pages
+import { About } from "./components/Pages/About";
+import { Home } from "./components/Pages/Home";
+
+
+const App = () => {
   
   return(
-    NavBar()
+    <BrowserRouter>
+      <Routes>
+        <Route
+          exact={true}
+          path="/" 
+          element={PageWrapper(Home)}
+        />
+
+        <Route 
+          path="/about" 
+          element={PageWrapper(About)}
+        />
+      </Routes>
+      
+    </BrowserRouter>
   )
-  
-  
 }
 
 export default App;
